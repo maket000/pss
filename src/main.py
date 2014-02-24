5a27f0059abbe3838c1ac5d6891c19be624064d4
maket <nmunce@gmail.com>
2014-02-23 20:03:19 -0500

    Added size property to rectangles, created object manager, added property panel to display properties of a iven object

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame, math
+from random import randint
 from pygame import *
 
 class Vector2:
@@ -21,25 +22,28 @@ class Vector2:
         return math.sqrt(this.x**2 + this.y**2)
     def angle(this):
         return math.atan2(this.y, this.x)
-    def getComp(this):
+    def get(this):
         return (this.x, this.y)
-    def getiComp(this):
+    def geti(this):
         return (int(this.x), int(this.y))
 
 class Object:
-    def __init__(this, position, velocity):
-        this.position = position
+    def __init__(this, position, velocity, size): #size is (w,h)
+        this.position = position # might be totally replaced by this.rect.center
         this.velocity = velocity
+        this.rect = pygame.Rect((0,0), size)
+        this.rect.center = this.position.get()
     def update(this):
         this.velocity += ACCEL
         this.position += this.velocity
+        this.rect.center = this.position.get()
     def draw(this):
-        screen.set_at(this.position.getiComp(), (255,255,255))
+        pygame.draw.rect(screen, (0,0,0), this.rect, 1)
 
 class ObjectManager:
     def __init__(this):
         this.objects = {}
-        this.nextindex = 0
+        this.nextindex = 1
     def update(this):
         for o in this.objects:
             this.objects[o].update()
@@ -49,40 +53,188 @@ class ObjectManager:
     def add(this, obj):
         this.objects[this.nextindex] = obj
         this.nextindex += 1
-        
+    def click(this, pos):
+        for o in this.objects:
+            if this.objects[o].rect.collidepoint(pos):
+                return o
+        return False
+
+class Textbox:
+    def __init__(this, rect, contents):
+        this.rect = rect
+        this.textpos = (rect.left+2, rect.top+1)
+        this.contents = contents
+        this.on = False
+        this.cursor = 0
+        this.render()
+    def assault(this, text):
+        this.contents = this.contents[:cursor]+text+this.contents[cursor:]
+        this.render()
+    def backspace(this):
+        if cursor:
+            this.contents = this.contents[:cursor-1]+this.contents[cursor:]
+            this.cursor -= 1
+        this.render()
+    def delete(this):
+        if cursor != len(this.contents)-1:
+            this.contents = this.contents[:cursor]+this.contents[cursor+1:]
+        this.render()
+    def update(this, contents):
+        this.contents = contents
+        this.render()
+    def render(this):
+        this.rendered = fonts["textbox"].render(this.contents, 1, (0,0,0))
+    def draw(this):
+        pygame.draw.rect(screen, (0,0,0), this.rect, 1)
+        screen.blit(this.rendered, this.textpos)
+    
+
+class Properter:
+    def __init__(this):
+        this.obj = False
+        this.textbox = {}
+        this.textbox["position.x"] = Textbox(pygame.Rect((30,100), (40,17)), "")
+        this.textbox["position.y"] = Textbox(pygame.Rect((80,100), (40,17)), "")
+        this.textbox["velocity.x"] = Textbox(pygame.Rect((30,120), (40,17)), "")
+        this.textbox["velocity.y"] = Textbox(pygame.Rect((80,120), (40,17)), "")
+        this.textboxon = False
+    def focus(this, obj):
+        this.obj = obj
+        this.update()
+    def updateTextBox(this, key):
+        this.textbox[key].update(str(round(eval("this.obj."+key), 1)))
+    def draw(this):
+        for t in this.textbox:
+            this.textbox[t].draw()
+    def click(this, click):
+        for t in this.textbox:
+            if this.textbox[t].rect.collidepoint(click):
+                this.textbox[t].on = True
+                this.textboxon = t
+    def keyit(this, event):
+        if event.key == 8:
+            this.textbox[textboxon].backspace()
+        elif event.key == 127:
+            this.textbox[textboxon].delete()
+        else:
+            this.textbox[textboxon].assault(inp)
+    def update(this):
+        for t in this.textbox:
+            this.updateTextBox(t)
+
 
 #Initializations
 pygame.init()
-pygame.display.set_icon(pygame.image.load("img/icon.ico"))
+pygame.display.set_icon(pygame.image.load("res/img/icon.ico"))
 dimensions = swidth, sheight = 1024, 600
 screen = pygame.display.set_mode(dimensions)
 clock = pygame.time.Clock()
 
+fonts = {}
+fonts["textbox"] = pygame.font.Font("res/font/Courier.ttf", 12)
+
 FPS = 30
-PMSCALE = 10.0
 
 ACCEL = Vector2(0, 9.8/FPS)
 
 
 objectmanager = ObjectManager()
-objectmanager.add( Object(Vector2(150,600), Vector2(4,-10)) )
-objectmanager.add( Object(Vector2(140,600), Vector2(4,-11)) )
-objectmanager.add( Object(Vector2(130,600), Vector2(4,-12)) )
-objectmanager.add( Object(Vector2(120,600), Vector2(4,-13)) )
-objectmanager.add( Object(Vector2(110,600), Vector2(4,-14)) )
-objectmanager.add( Object(Vector2(100,600), Vector2(4,-15)) )
+properter = Properter()
 
 running = True
+paused = False
+focus = False
+ticker = 0
 while running:
-    #input
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
+    ticker += 1
+    screen.fill((255,255,255))
     #update
-    objectmanager.update()
+    if paused:
+        #pause update logic
+        if focus:
+            #focused paused
+            #input
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == KEYDOWN:
+                    if properter.textboxon:
+                        properter.keyit(event)
+                    elif event.key == K_p:
+                        paused = not paused
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        if not properter.click(event.pos):
+                            newfocus = objectmanager.click(event.pos)
+                            if newfocus == focus:
+                                focus = False
+                            else:
+                                focus = newfocus
+                                if focus:
+                                    properter.focus(objectmanager.objects[focus])
+            #draw
+            properter.draw()
+        else:
+            #unfocused paused
+            #input
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_p:
+                        paused = not paused
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        focus = objectmanager.click(event.pos)
+                        if focus:
+                            properter.focus(objectmanager.objects[focus])
+    else:
+        if focus:
+            #focused unpaused
+            #input
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                if event.type == KEYDOWN:
+                    if properter.textboxon:
+                        properter.keyit(event)
+                    elif event.key == K_p:
+                        paused = not paused
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        if not properter.click(event.pos):
+                            newfocus = objectmanager.click(event.pos)
+                            if newfocus == focus:
+                                focus = False
+                            else:
+                                focus = newfocus
+                                if focus:
+                                    properter.focus(objectmanager.objects[focus])
+            properter.update()
+            #draw
+            properter.draw()
+            if ticker%30==0:
+                objectmanager.add( Object(Vector2(512,300), Vector2(randint(-10,10), -3), (randint(10,50), randint(10,50))) )
+                properter.focus(objectmanager.objects[objectmanager.nextindex-1])
+            objectmanager.update()
+        else:
+            #unpaused update logic
+            #input
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_p:
+                        paused = not paused
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        focus = objectmanager.click(event.pos)
+                        if focus:
+                            properter.focus(objectmanager.objects[focus])
+            if ticker%30==0:
+                objectmanager.add( Object(Vector2(512,300), Vector2(randint(-10,10), -3), (randint(10,50), randint(10,50))) )
+            objectmanager.update()
     
-    #draw
-    screen.fill((0,0,0))
     objectmanager.draw()
     pygame.display.flip()
     clock.tick(FPS)
